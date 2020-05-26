4e461e8b3324a0da02806b1d2b856dbcd316dd7f
yazz <15998949+diabolical-ninja@users.noreply.github.com>
2020-05-26 11:27:16 +0000

    Update to fix package download & unnecessary assignments

--- build_domain_client_library.sh
+++ build_domain_client_library.sh
@@ -4,7 +4,6 @@ echo "Generating client..."
 
 # Location of the Swagger Config
 SWAGGER_DOCUMENT="https://developer.domain.com.au/docs/media/public-adapter-v1.json"
-SWAGGER_DOCUMENT="http://petstore.swagger.io/v2/swagger.json"
 
 JSON_FMT='{"options":{"packageName": "domain-client"},"swaggerUrl":"%s"}'
 JSON_REQUEST=$(printf "$JSON_FMT" $SWAGGER_DOCUMENT)
@@ -13,20 +12,18 @@ response=$(
     curl -X POST \
         -H "content-type:application/json" \
         -d "$JSON_REQUEST" \
-        https://generator.swagger.io/api/gen/clients/ruby
+        https://generator.swagger.io/api/gen/clients/python
     )
 echo "Generating client...Done"
-echo $response
 
 
 LINK_URL=$(echo $response | grep -Eo 'https:[^"]+')
-
 CLIENT_NAME="domain-client"
+
 echo "Downloading $CLIENT_NAME..."
-
-
-curl -X GET $LINK_URL
+curl $LINK_URL -o $CLIENT_NAME.zip
 echo "Downloading $CLIENT_NAME...Done"
 
 echo "Unzip Package $CLIENT_NAME..."
-unzip $CLIENT_NAME.zip
+unzip $CLIENT_NAME.zip -q
+echo "Unzip Package $CLIENT_NAME...Done"
