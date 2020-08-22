94ceb8df454508e34958d34e3ce08ed84f1ec154
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2020-08-22 21:25:49 +1000

    Update formatting

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
+"""General utility functions."""
+
+import typing
 from datetime import date, datetime
 
 
-def json_serial(obj):
-    """JSON serializer for objects not serialisable by default json code
+def json_serial(obj: typing.Any) -> typing.Any:
+    """JSON serializer for objects not serialisable by default json code.
 
     Args:
         obj (any): Object that JSON cannot serialise
@@ -18,13 +21,30 @@ def json_serial(obj):
     raise TypeError("Type %s not serialisable" % type(obj))
 
 
-def chunks(lst, n):
-    """Yield successive n-sized chunks from list"""
+def chunks(lst: list, n: int) -> list:
+    """Yield successive n-sized chunks from list.
+
+    Args:
+        lst (list): List to chunk up
+        n (int): Number of elements to have in each chunk
+
+    Yields:
+        Iterator[list]: Chunks of size n
+    """
     for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+        yield lst[i: i + n]
 
 
-def extend_dictionary(base_dict, extender_dict, key_name):
-    """Adds keys from one dictionary to another"""
+def extend_dictionary(base_dict: dict, extender_dict: dict, key_name: str) -> dict:
+    """Adds keys from one dictionary to another.
+
+    Args:
+        base_dict (dict): Base dictionary to be extended
+        extender_dict (dict): Dictionary whose keys will extend base
+        key_name (str): The key to be added to base
+
+    Returns:
+        dict: Base dictionary plus additional key
+    """
     base_dict[key_name] = extender_dict
     return base_dict
