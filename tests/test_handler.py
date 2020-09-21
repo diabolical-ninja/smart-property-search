2396a82b629aa90cfab7ea7333a220af21f06978
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2020-09-22 00:20:59 +1000

    Update to tests to better assess functionality

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -12,22 +12,45 @@ sys.path.append(os.path.join(os.getcwd(), "src"))
 import handler  # noqa
 
 
-test_files = os.listdir(os.path.join(Path(__file__).parent, "test_handler_payloads"))
+success_test_files = os.listdir(os.path.join(Path(__file__).parent, "test_handler_payloads", "successes"))
+failure_test_files = os.listdir(os.path.join(Path(__file__).parent, "test_handler_payloads", "failures"))
 
 
-@pytest.mark.parametrize("test_file", test_files)
-def test_search(test_file: dict) -> None:
-    """Integration tests for smart search functionality.
-    
-    Notes: 
+@pytest.mark.parametrize("test_file", success_test_files)
+def test_search_success(test_file: dict) -> None:
+    """Integration tests for successful smart search functionality.
+
+    Notes:
         - Mocks lambda context with None (null)
         - Uses a sample event with all the fluff from API Gateway
 
     Args:
-        test_file (dict): Sample payloads
+        test_file (dict): Sample payloads that should work successfully
     """
-    with open(os.path.join(Path(__file__).parent, "test_handler_payloads", test_file)) as f:
+    with open(os.path.join(Path(__file__).parent, "test_handler_payloads", "successes", test_file)) as f:
         test_event = json.load(f)
 
     test_result = handler.search(test_event, None)
     assert isinstance(test_result, dict)
+    assert test_result["statusCode"] == 200
+    assert "body" in test_result
+
+
+@pytest.mark.parametrize("test_file", failure_test_files)
+def test_failures_success(test_file: dict) -> None:
+    """Integration tests for unsuccessful smart search functionality.
+
+    Notes:
+        - Mocks lambda context with None (null)
+        - Uses a sample event with all the fluff from API Gateway
+
+    Args:
+        test_file (dict): Sample payloads that should not
+    """
+    with open(os.path.join(Path(__file__).parent, "test_handler_payloads", "failures", test_file)) as f:
+        test_event = json.load(f)
+
+    test_result = handler.search(test_event, None)
+    assert isinstance(test_result, dict)
+    assert test_result["statusCode"] == 500
+    assert "error" in test_result
